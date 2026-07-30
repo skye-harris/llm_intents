ccefcd0c6392cb88b992788c37369d364646b606
Skye Harris <skyeserenaharris@gmail.com>
2026-07-30 13:52:30 +0800

    Retrieve tools from all platforms

--- custom_components/llm_intents/config_flow.py
+++ custom_components/llm_intents/config_flow.py
@@ -548,12 +548,10 @@ async def enumerate_tools(hass: HomeAssistant) -> list[llm.Tool]:
     tools = []
     apis = llm.async_get_apis(hass)
     for api in apis:
-        # For simplicity lets just enumerate directly from assist, as otherwise our own internal filtering may get in the way of this
-        if api.name == "Assist":
-            api_instance = await api.async_get_api_instance(
-                LLMContext(DOMAIN, None, None, "conversation", None),
-            )
-            tools.extend(api_instance.tools)
+        api_instance = await api.async_get_api_instance(
+            LLMContext(DOMAIN, None, None, "conversation", None),
+        )
+        tools.extend(api_instance.tools)
 
     return tools
 
